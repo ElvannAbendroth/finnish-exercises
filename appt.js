16d83b5176615e88cf8ccd6c56f2e6db2d6166d4
Elvann <elvann.abendroth@gmail.com>
2018-08-29 21:08:11 -0400

    help! tried to randomize verbs but can't select random key form dicitonary?

--- appt.js
+++ appt.js
@@ -1,6 +1,13 @@
 const Verb = require('./models/Verb');
 const Conjugation = require('./models/Conjugation');
 
+// import from Conjugation
+const pronouns = ["Minä", "Sinä", "Hän", "Me", "Te", "He"];
+const verbsPresent = {
+    "rakastaa": ["rakastan", "rakastat", "rakastaa", "rakastamme", "rakastatte", "rakastavat"],
+    "syödä": ["syön", "syöt", "syö", "syömme", "syötte", "syövat"]
+};
+
 var myVerb = new Verb("rakastaa", "I");
 var myConjugatedVerb = new Conjugation(myVerb, "Minä", "present");
 
@@ -23,6 +30,14 @@ var syodaPresent = new Conjugation(syoda, "Sinä", "present");
 
 console.log(syodaPresent.getPronoun() + " " + syodaPresent.conjugatePresent());
 
-//var randomVerb = new Verb(verbsPresent[Math.floor(Math.random()*verbsPresent.length)], "I")
-//console.log(randomVerb.getPronoun() + " " + randomVerb.conjugatePresent());
+var randomVerb = verbsPresent[Math.floor(Math.random()*verbsPresent.length)];
+var randomPronoun = pronouns[Math.floor(Math.random()*pronouns.length)];
+
+//var randomConjugatedVerb = new Conjugation(randomVerb, randomPronoun, "present");
+//console.log(randomConjugatedVerb.getPronoun() + " " + randomConjugatedVerb.conjugatePresent());
+
+console.log(randomVerb);
+console.log(verbsPresent["rakastaa"]);
+
+//how to select a random key from a dictionary from an index number??? 
 
