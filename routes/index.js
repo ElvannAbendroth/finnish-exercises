5707614eb9202d95e6df7acfb397cf89cb6b2330
Pekka Astala <pekka.astala@gmail.com>
2018-09-11 22:21:32 +0300

    sendFile based routes

--- routes/index.js
+++ routes/index.js
@@ -4,6 +4,7 @@ const Verb = require('../models/Verb');
 const Question = require('../models/Question');
 const pronouns = require('../models/PronounEnum');
 const verbsPresent = require('../models/verbData');
+var path = require('path');
 
 //Wild functions
 function buildRandomQuestion() {
@@ -29,16 +30,14 @@ router.get('/question', function(req, res, next) {
 /* GET blank */
 
 router.get('/blank', function(req, res, next) {
-  res.redirect('blank.html');
-  //res.send(buildRandomQuestion()); 
+  res.sendFile(path.join(__dirname, '../public/blank.html'));
 });
 
 
 /* GET present-exercises */
 
 router.get('/present-exercises', function(req, res, next) {
-  res.redirect('present-exercises.html');
-  //res.send(buildRandomQuestion()); 
+  res.sendFile(path.join(__dirname, 'present-exercises.html'));
 });
 
 
