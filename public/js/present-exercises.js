507ef41b1fea09b2ea214b7967e172ecbb2867fd
Elvann <elvann.abendroth@gmail.com>
2018-09-30 20:53:48 -0400

    connected backend with frontend.  Still need to do the reset-score button

--- public/js/present-exercises.js
+++ public/js/present-exercises.js
@@ -1,7 +1,13 @@
 window.elvann = { };
+window.elvann.success = 0;
 window.elvann.missed = 0;
 window.elvann.totalExercises = 0;
-window.elvann.success = 0;
+
+function resetScore (){
+    window.elvann.success = 0;
+    window.elvann.missed = 0;
+    window.elvann.totalExercises = 0;
+}
 
 function fetchQuestion(){
     $.get('/question', function(data, status) {
@@ -11,11 +17,11 @@ function fetchQuestion(){
     const infinitive = data.verb.infinitive;
     const translation = data.verb.translation;
     
-    $('#questionPrompt').html(pronoun);
+    $('#pronoun').html(pronoun);
     $('#infinitive').html(infinitive);
-    $("#answerInput").attr("placeholder", infinitive);
-    $('#answerInput').val('');
-    $('#answerHelp').html("Translation: " + translation);
+    $("#verb-input").attr("placeholder", infinitive);
+    $('#verb-input').val('');
+    $('#translation').html(translation);
 
     window.elvann.conjugatedVerb = conjugatedVerb;
     window.elvann.noMistake = 0; // resets every round
@@ -24,24 +30,20 @@ function fetchQuestion(){
 }
 
 function updateStats(numberCorrect, numberMissed, numberTotal) {
-    $('#success').html('<i class="fa fa-check"></i>' + ' Success: ' + numberCorrect);
-    $('#missed').html('<i class="fa fa-times"></i>' + ' Missed: ' + numberMissed);
-    $('#totalExercises').html('<i class="fa fa-heart"></i>' + ' Total: ' + numberTotal);
+    $('#success').html(numberCorrect);
+    $('#missed').html(numberMissed);
+    $('#totalExercises').html(numberTotal);
 }
 
 $( document ).ready(function() {
     /* This code gets executed when the page loads */ 
 
     fetchQuestion();
-
-    const pageTitle = "Harjoitukset Preesensiksi";
-    $('#breadTitle').html(pageTitle);
-    $('#pageTitle').html(pageTitle);
     updateStats(window.elvann.success, window.elvann.missed, window.elvann.totalExercises);
 
     $('#answerForm').submit((event) => {
         event.preventDefault();
-        const answer = $('#answerInput').val();
+        const answer = $('#verb-input').val();
         const isCorrect = answer === window.elvann.conjugatedVerb;
         if (isCorrect) {
             $('#verb-input').addClass('correct', 1000, () => {
@@ -50,7 +52,7 @@ $( document ).ready(function() {
             });
             //TODO: create a step that replicates the alert effect of waiting from a user input, and will clear the exercise
             window.elvann.totalExercises += 1;
-            $('#totalExercises').html('Total Exercises: ' + window.elvann.totalExercises);
+            $('#totalExercises').html(window.elvann.totalExercises);
             $('#answerMissed').html('');
             if (window.elvann.noMistake < 1){
                 window.elvann.success += 1;
@@ -60,11 +62,11 @@ $( document ).ready(function() {
             $('#verb-input').addClass('wrong', 1000, () => {
                 $('#verb-input').removeClass('wrong', 0);
             });
-            //TODO: clear field
+            $('#verb-input').val('');
             if (window.elvann.noMistake < 1){
                 window.elvann.missed +=1;
                 updateStats(window.elvann.success, window.elvann.missed, window.elvann.totalExercises);
-            } if (window.elvann.noMistake >= 1){
+            } if (window.elvann.noMistake >= 2){
                 $('#answerMissed').html('Answer: ' + window.elvann.conjugatedVerb);
             }
             window.elvann.noMistake += 1;
