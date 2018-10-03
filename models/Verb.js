8b4e5f43da069482bcfdf134f046b0b5303ea682
Elvann <elvann.abendroth@gmail.com>
2018-10-03 00:27:27 -0400

    added more verbs to the practice list and added a verbtype attribute

--- models/Verb.js
+++ models/Verb.js
@@ -3,15 +3,16 @@ class Verb {
     constructor(formsList) {
         this.infinitive = formsList[0];
         this.translation = formsList[1];
+        this.type = formsList[2];
     
         this.conjugations = {
             "present": {
-                "minä": formsList[2],
-                "sinä": formsList[3],
-                "hän": formsList[4],
-                "me": formsList[5],
-                "te": formsList[6],
-                "he": formsList[7],
+                "minä": formsList[3],
+                "sinä": formsList[4],
+                "hän": formsList[5],
+                "me": formsList[6],
+                "te": formsList[7],
+                "he": formsList[8],
             }
         };
     }
--- models/verbData.js
+++ models/verbData.js
@@ -1,14 +1,22 @@
 const verbsPresent = [
-    ["rakastaa", "to love", "rakastan", "rakastat", "rakastaa", "rakastamme", "rakastatte", "rakastavat"],
-    ["syödä", "to eat", "syön", "syöt", "syö", "syömme", "syötte", "syövät"],
-    ["haluta", "to want", "haluan", "haluat", "haluaa", "haluamme", "haluatte", "haluavat"],
-    ["olla", "to be", "olen", "olet", "on", "olemme", "olette", "ovat"],
-    ["puhua", "to speak", "puhun", "puhut", "puhuu", "puhumme", "puhutte", "puhuvat"],
-    ["kirjoittaa", "to write", "kirjoitan", "kirjoitat", "kirjoittaa", "kirjoitamme", "kirjoitatte", "kirjoittavat"],
-    ["tehdä", "to make", "teen", "teet", "tekee", "teemme", "teette", "tekevät"],
-    ["ottaa", "to take", "otan", "otat", "ottaa", "otamme", "otatte", "ottavat"],
-    ["kaivata", "to miss", "kaipaan", "kaipaat", "kaipaa", "kaipaamme", "kaipaatte", "kaipaavat"]
+    ["rakastaa", "to love", "I", "rakastan", "rakastat", "rakastaa", "rakastamme", "rakastatte", "rakastavat"],
+    ["syödä", "to eat", "II", "syön", "syöt", "syö", "syömme", "syötte", "syövät"],
+    ["haluta", "to want", "IV", "haluan", "haluat", "haluaa", "haluamme", "haluatte", "haluavat"],
+    ["olla", "to be", "III", "olen", "olet", "on", "olemme", "olette", "ovat"],
+    ["puhua", "to speak", "I", "puhun", "puhut", "puhuu", "puhumme", "puhutte", "puhuvat"],
+    ["kirjoittaa", "to write", "I", "kirjoitan", "kirjoitat", "kirjoittaa", "kirjoitamme", "kirjoitatte", "kirjoittavat"],
+    ["tehdä", "to make", "II", "teen", "teet", "tekee", "teemme", "teette", "tekevät"],
+    ["ottaa", "to take", "I", "otan", "otat", "ottaa", "otamme", "otatte", "ottavat"],
+    ["kaivata", "to miss", "IV", "kaipaan", "kaipaat", "kaipaa", "kaipaamme", "kaipaatte", "kaipaavat"],
+
+    ["nähdä", "to see", "", "näen", "näet", "näkee", "näemme", "näette", "näkevät"],
+    ["mennä", "to go", "", "menen", "menet", "menee", "menemme", "menette", "menevät"],
+    ["tulla", "to come", "", "tulen", "tulet", "tulee", "tulemme", "tulette", "tulevat"],
+    ["tykätä", "to like", "", "tykkään", "tykkäät", "tykkää", "tykkäämme", "tykkäätte", "tykkäävät"]
 
 ];
+
+//add a list for irregular verbs?  Or a boolean?
+//add attribute for consonnant gradations?
 
 module.exports = verbsPresent;
