76b4be66419f86c07955741727b9c0c841d64dca
Pekka Astala <pekka.astala@gmail.com>
2018-10-07 20:30:48 +0300

    Added offsets to verb data storing

--- models/Verb.js
+++ models/Verb.js
@@ -4,15 +4,26 @@ class Verb {
         this.infinitive = formsList[0];
         this.translation = formsList[1];
         this.type = formsList[2];
+
+        const presentOffset = 3;
+        const pastOffset = 9;
     
         this.conjugations = {
             "present": {
-                "minä": formsList[3],
-                "sinä": formsList[4],
-                "hän": formsList[5],
-                "me": formsList[6],
-                "te": formsList[7],
-                "he": formsList[8],
+                "minä": formsList[presentOffset + 0],
+                "sinä": formsList[presentOffset + 1],
+                "hän": formsList[presentOffset + 2],
+                "me": formsList[presentOffset + 3],
+                "te": formsList[presentOffset + 4],
+                "he": formsList[presentOffset + 5],
+            },
+            "past": {
+                "minä": formsList[pastOffset + 0],
+                "sinä": formsList[pastOffset + 1],
+                "hän": formsList[pastOffset + 2],
+                "me": formsList[pastOffset + 3],
+                "te": formsList[pastOffset + 4],
+                "he": formsList[pastOffset + 5],
             }
         };
     }
--- models/verbData.js
+++ models/verbData.js
@@ -1,19 +1,69 @@
 const verbsPresent = [
-    ["rakastaa", "to love", "I", "rakastan", "rakastat", "rakastaa", "rakastamme", "rakastatte", "rakastavat"],
-    ["syödä", "to eat", "II", "syön", "syöt", "syö", "syömme", "syötte", "syövät"],
-    ["haluta", "to want", "IV", "haluan", "haluat", "haluaa", "haluamme", "haluatte", "haluavat"],
-    ["olla", "to be", "III", "olen", "olet", "on", "olemme", "olette", "ovat"],
-    ["puhua", "to speak", "I", "puhun", "puhut", "puhuu", "puhumme", "puhutte", "puhuvat"],
-    ["kirjoittaa", "to write", "I", "kirjoitan", "kirjoitat", "kirjoittaa", "kirjoitamme", "kirjoitatte", "kirjoittavat"],
-    ["tehdä", "to make", "II", "teen", "teet", "tekee", "teemme", "teette", "tekevät"],
-    ["ottaa", "to take", "I", "otan", "otat", "ottaa", "otamme", "otatte", "ottavat"],
-    ["kaivata", "to miss", "IV", "kaipaan", "kaipaat", "kaipaa", "kaipaamme", "kaipaatte", "kaipaavat"],
-
-    ["nähdä", "to see", "", "näen", "näet", "näkee", "näemme", "näette", "näkevät"],
-    ["mennä", "to go", "", "menen", "menet", "menee", "menemme", "menette", "menevät"],
-    ["tulla", "to come", "", "tulen", "tulet", "tulee", "tulemme", "tulette", "tulevat"],
-    ["tykätä", "to like", "", "tykkään", "tykkäät", "tykkää", "tykkäämme", "tykkäätte", "tykkäävät"]
-
+    [
+        "rakastaa", "to love", "I", 
+        "rakastan", "rakastat", "rakastaa", "rakastamme", "rakastatte", "rakastavat",
+        "rakastin", "rakastit", "rakasti", "rakastimme", "rakastitte", "rakastivat"
+    ],
+    [
+        "syödä", "to eat", "II", 
+        "syön", "syöt", "syö", "syömme", "syötte", "syövät",
+        "söin", "söit", "söi", "söimme", "söitte", "söivät",
+    ],
+    [
+        "haluta", "to want", "IV", 
+        "haluan", "haluat", "haluaa", "haluamme", "haluatte", "haluavat",
+        "", "", "", "", "", ""
+    ],
+    [
+        "olla", "to be", "III", 
+        "olen", "olet", "on", "olemme", "olette", "ovat",
+        "", "", "", "", "", ""
+    ],
+    [
+        "puhua", "to speak", "I", 
+        "puhun", "puhut", "puhuu", "puhumme", "puhutte", "puhuvat",
+        "", "", "", "", "", ""
+    ],
+    [
+        "kirjoittaa", "to write", "I", 
+        "kirjoitan", "kirjoitat", "kirjoittaa", "kirjoitamme", "kirjoitatte", "kirjoittavat",
+        "", "", "", "", "", ""
+    ],
+    [
+        "tehdä", "to make", "II", 
+        "teen", "teet", "tekee", "teemme", "teette", "tekevät",
+        "", "", "", "", "", ""
+    ],
+    [
+        "ottaa", "to take", "I", 
+        "otan", "otat", "ottaa", "otamme", "otatte", "ottavat",
+        "", "", "", "", "", ""
+    ],
+    [
+        "kaivata", "to miss", "IV", 
+        "kaipaan", "kaipaat", "kaipaa", "kaipaamme", "kaipaatte", "kaipaavat",
+        "", "", "", "", "", ""
+    ],
+    [
+        "nähdä", "to see", "", 
+        "näen", "näet", "näkee", "näemme", "näette", "näkevät",
+        "", "", "", "", "", ""
+    ],
+    [
+        "mennä", "to go", "", 
+        "menen", "menet", "menee", "menemme", "menette", "menevät",
+        "", "", "", "", "", ""
+    ],
+    [
+        "tulla", "to come", "", 
+        "tulen", "tulet", "tulee", "tulemme", "tulette", "tulevat",
+        "", "", "", "", "", ""
+    ],
+    [
+        "tykätä", "to like", "", 
+        "tykkään", "tykkäät", "tykkää", "tykkäämme", "tykkäätte", "tykkäävät",
+        "", "", "", "", "", ""
+    ]
 ];
 
 //add a list for irregular verbs?  Or a boolean?
