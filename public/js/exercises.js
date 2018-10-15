95a6433e1560a85e8ea77e132b62b15c6a8937c1
Elvann <elvann.abendroth@gmail.com>
2018-10-14 23:32:03 -0400

    refractored to App and SideBar classes, changed button behaviours to jQuery bindings.  help menu button only works after opening and closing the menu :thinking:

--- public/js/exercises.js
+++ public/js/exercises.js
@@ -1,9 +1,73 @@
 class App {
 
+    
+    run(){
+        
+        const _sideNav = new SideNav('mainNav')
+        _sideNav.bindEvents();
+        _sideNav.closeNav();
+    }
 
+}
+
+class SideNav {
+
+    constructor (wrapperID) {
+        this.wrapperID = wrapperID;
+    }
+
+    bindEvents(){
+        $("#menuIcon").click(() => {this.openNav();console.log("opened menu")});
+        $("#helpIcon").click(() => {this.openNav('help');console.log("opened help")});
+        $("#closeIcon").click(() => {this.closeNav();console.log("closed menu")});
+        $("#pronounButton").click(() => {this.navigationContent('pronoun');console.log("opened pronouns")});
+        $("#consonantButton").click(() => {this.navigationContent('consonant');console.log("opened consonant")});
+        $("#conjugationButton").click(() => {this.navigationContent('conjugation');console.log("opened conjugation")});
+        
+        $("#presentButton").click(() => {setTense('present');console.log("set present!");this.closeNav();console.log("closed menu")});
+        $("#pastButton").click(() => {setTense('past');console.log("set past!");this.closeNav();console.log("closed menu")});
+        $("#conditionalButton").click(() => {setTense('conditional');console.log("set conditional!");this.closeNav();console.log("closed menu")});
+
+        $("#resetButton").click(() => {resetScore();console.log("reset score");this.closeNav();console.log("closed menu")});
+        $("#aboutButton").click(() => {this.navigationContent('about');console.log("opened about")});
+        
+        $("#returnButton").click(() => {this.navigationContent('default');console.log("returned to main menu")});
+
+    }
+
+    openNav(key = 'default') {
+        
+        this.navigationContent(key);
+        document.getElementById(this.wrapperID).style.width = "100%";
+    }
+    
+    /* Set the width of the side navigation to 0 */
+    closeNav() {
+        document.getElementById(this.wrapperID).style.width = "0";
+    };
+
+    /*openHelp() {
+        verbHelp.updateHtml();
+        document.getElementById(this.wrapperID).style.width = "100%";
+    }*/
+    
+    navigationContent(key = 'default') {
+        if (key === 'help'){
+            
+            verbHelp.updateHtml();
+        }
+        else{
+            const html = navigationHtmls[key];
+            $("#" + this.wrapperID).html(html);
+        }
+       
+        this.bindEvents();
+
+    }
 
 
 }
+
 
 
 class ScoreBoard {
@@ -88,8 +152,9 @@ class VerbHelp {
     updateInfo (infinitive, translation, type) {
         //const verbTypeTitle = 'verbityyppi ' + (type || 'unknown');
         this.html = `<div class="d-flex p-4 justify-content-between align-middle">
-            <h1 class="align-bottom"><i class="material-icons p-2 align-middle">help</i>help</h1>    
-            <a class="close align-middle closebtn" onclick="closeNav()" ><i class="material-icons p-1 ">clear</i></a>        </div>
+                <h1 class="align-bottom"><i class="material-icons p-2 align-middle">help</i>help</h1>    
+                <a id="closeIcon" class="close align-middle closebtn"><i  class="material-icons p-1 ">clear</i></a>
+            </div>
     
         <div class="scroll p-4">
             <p>Here's a little bit of info to help you help you with this exercise.</p>
@@ -182,7 +247,7 @@ class QuestionBox {
                     <h2 id="pronoun" class="p-2">${pronoun}</h2>
                     
                 </div> 
-                <a class="p-2" data-toggle="modal" onclick="openHelp()"><i class="material-icons">help</i></a>
+                <a id="helpIcon" class="p-2"><i class="material-icons">help</i></a>
                 <a onclick="fetchQuestion()"><i class="material-icons p-2">refresh</i></a>
 
             </div>
@@ -223,17 +288,6 @@ const questionBox = new QuestionBox('question-card', submitQuestion);
 window.score = { };
 window.score.tense = 'present';
 
-
-
-function closeModal(modalID, fadeOutAnimationLength){    
-    $(modalID).addClass('fadeOut');
-
-    setTimeout(() => {
-        $(modalID).modal('hide');
-        $(modalID).removeClass('fadeOut');
-    }, fadeOutAnimationLength);
-}
-
 function setTense(tense) {
     window.score.tense = tense;
     fetchQuestion();
@@ -245,7 +299,7 @@ function resetScore(){
 }
 
 function fetchQuestion() {
-    closeNav();
+    
     const tense = window.score.tense || 'present';
     $.get('/question?tense=' + tense, function(data, status) {
         /* this code is executed when get requests to my-url returns with a response */
@@ -258,7 +312,7 @@ function fetchQuestion() {
         verbHelp.updateInfo(infinitive, translation, verbType);
 
         questionBox.setQuestion(tense, pronoun, infinitive);
-        
+;        
         window.score.conjugatedVerb = conjugatedVerb;
         window.score.noMistake = 0; // resets every round
     });
@@ -291,10 +345,116 @@ function submitQuestion() {
     }
 }
 
+const navigationHtmls = {
+    'default': `<div class="d-flex p-4 justify-content-between">
+                    <h1><i class="material-icons p-2 align-middle">menu</i>menu</h1>
+                    <a id="closeIcon" class="close align-middle closebtn"><i  class="material-icons p-1 ">clear</i></a>
+                </div>
+                        
+                <div class="d-flex flex-column">
+
+                    <h2 class="p-2"><i class="material-icons p-2 align-middle">school</i>learn finnish</h2>
+                    <button id="pronounButton" data-dismiss="modal" class="btn btn-menu""><i class="material-icons p-2 align-middle">star</i>pronouns</button>
+                    <button id="consonantButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">star</i>consonant gradation</button>
+                    <button id="conjugationButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">star</i>verb conjugaison</button> 
+                    
+
+
+                    <h2 class="p-2"><i class="material-icons p-2 align-middle">settings</i>exercise settings</h2>  
+                    <button id="presentButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">check</i>present tense</button>
+                    <button id="pastButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">check</i>past tense</button>
+                    <button id="conditionalButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">check</i>conditional mood</button>
+                    <hr>
+                    <button id="resetButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">highlight_off</i>reset score</button>
+                    <button id="aboutButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">info</i>about</button>
+                    <br>
+                </div>`,
+
+    'consonant': `<div class="d-flex p-4 justify-content-between">
+                    <h1 class="align-middle"><i class="material-icons p-2 align-middle">star</i>consonant gradation</h1>    
+                    <a id="closeIcon" class="close align-middle closebtn"><i class="material-icons p-1 ">clear</i></a>                </div>
+
+                <div class="p-4">
+                    <p>Some Finnish words go under <i>consonant gradation</i> when conjugated.  Here's a table containing the "strong > weak" consonant combinations to apply this gradation. </p>
+                    <ul>
+                        <li>pp > p</li>
+                        <li>tt > t</li>
+                        <li>kk ></li>
+                        <li>k > ø </li>
+                        <li>p > v</li>
+                        <li>t > d</li>
+                        <li>nk > ng</li>
+                        <li>nt > nn</li>
+                        <li>lt > ll</li>
+                        <li>rt > rr</li>
+                        <li>mp > mm</li>
+                    </ul>
+                </div>
+                <button id="returnButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">arrow_back_ios</i>return</button>
+                <br><br>`,
+    
+    'pronoun': `<div class="d-flex p-4 justify-content-between">
+                    <h1 class="align-middle"><i class="material-icons p-2 align-middle">star</i>pronouns</h1>    
+                    <a id="closeIcon" class="close align-middle closebtn"><i class="material-icons p-1 ">clear</i></a>                </div>
+
+                <div class="p-4">
+                <p>Here are the pronouns in Finnish as well as their english counterpart.  N.B. "he" is used 
+                    in formal situation instead of "hän"</p>
+                <ul>
+                    <li>minä - I</li>
+                    <li>sinä - you</li>
+                    <li>hän - he/she</li>
+                    <li>me - we</li>
+                    <li>te - you (pl.)</li>
+                    <li>he - they</li>
+                </ul>
+                </div>
+                <button id="returnButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">arrow_back_ios</i>return</button>
+                `,
+    
+    'about': `<div class="d-flex p-4 justify-content-between">
+                <h1 class="align-middle"><i class="material-icons p-2 align-middle">info</i>about</h1>    
+                <a id="closeIcon" class="close align-middle closebtn"><i class="material-icons p-1 ">clear</i></a>
+            </div>
+
+            <div class="p-4">
+                <p>"Opetellaan Suomea" is a web application designed to help you learn & practice Finnish.</p>
+                <p>In the current exercise, you are given verb in it’s infinitive form, and you must conjugate it to the present tense accordingly to a given pronoun.  The translate window gives you a few tips on how to conjugate verb in Finnish.</p>
+                <p> Questions?<br>
+                <a href="http://www.elvann.com/contact" target="_blank">elvann.com/contact</a></p><br>     
+                <button id="returnButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">arrow_back_ios</i>return</button>
+            </div>`,
+    
+    'conjugation': ` <div class="d-flex p-4 justify-content-between">
+                        <h1 class="align-middle"><i class="material-icons p-2 align-middle">star</i>verb conjugation</h1>    
+                        <a id="closeIcon" class="close align-middle closebtn"><i class="material-icons p-1 ">clear</i></a>                    </div>
+
+                    <div class="p-4">
+
+                        <h2>conjugation stems</h2>
+                        <ul>
+                            <li>minä - n</li>
+                            <li>sinä - t</li>
+                            <li>hän - /double vowel</li>
+                            <li>me - mme</li>
+                            <li>te - tte</li>
+                            <li>he - -vat</li>
+                        </ul>
+
+                        
+                        
+                    </div>
+                    <button id="returnButton" class="btn btn-menu"><i class="material-icons p-2 align-middle">arrow_back_ios</i>return</button>
+                    <br><br>`
+};
+
+/* Set the width of the side navigation to 250px */
+
+
 $( document ).ready(function() {
     /* This code gets executed when the page loads */
-    //var app = new App();
-    //app.run();
+    const app = new App();
+    app.run();   
     
     fetchQuestion();
 
@@ -305,7 +465,7 @@ $( document ).ready(function() {
         }
     };
 
-    navigationContent();
+    
 
    
 });
