19e4bc22783a2956b5c92c20625b95cd4c0ca1c0
Elvann <elvann.abendroth@gmail.com>
2018-10-16 00:14:53 -0400

    refractored some of the content of VerbExercise class

--- public/js/verbexercise.js
+++ public/js/verbexercise.js
@@ -1,30 +1,38 @@
 class VerbExercise {
 
+    constructor(){
+        this.tense = window.score.tense || 'present';
+        this.pronoun;
+    }
+    
+    
     fetchQuestion() {
     
-        const tense = window.score.tense || 'present';
-        $.get('/question?tense=' + tense, function(data, status) {
+        const tense = this.tense;
+
+        $.get('/question?tense=' + this.tense, function(data, status) {
             /* this code is executed when get requests to my-url returns with a response */
             const pronoun = data.pronoun;
             const conjugatedVerb = data.verb.conjugations[data.tense][pronoun];
             const infinitive = data.verb.infinitive;
             const translation = data.verb.translation;
             const verbType = data.verb.type;
-    
+            
             app._verbHelp.updateInfo(infinitive, translation, verbType);
-    
-            app._questionBox.setQuestion(tense, pronoun, infinitive);
+            app._questionBox.setQuestion(tense, pronoun, infinitive); //sets HTMLs
             app._sideNav.bindEvents();
             window.score.conjugatedVerb = conjugatedVerb;
             window.score.noMistake = 0;
+            
         });
     }
 
+
+
     setTense(tense) {
 
-        window.score.tense = tense;
+        this.tense = tense;
         app._verbExercise.fetchQuestion();
     }
-
    
 }
